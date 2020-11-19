0cfb36be8e01c3a42d6bfe7288cbd6905890cf66
dlugi57 <dlugoszpeter@gmail.com>
2020-11-19 22:41:57 +0100

    DEV rule name model

--- src/main/java/com/web/poseidon/domain/RuleName.java
+++ src/main/java/com/web/poseidon/domain/RuleName.java
@@ -1,11 +1,62 @@
 package com.web.poseidon.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.sql.Timestamp;
-
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
-@Table(name = "rulename")
+@Table(name = "RuleName")
 public class RuleName {
-    // TODO: Map columns in data table RULENAME with corresponding java fields
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id", columnDefinition = "TINYINT")
+    Integer id;
+
+
+    @Column(length = 125)
+    @NotNull
+    @Size(max = 125, message = "name should be maximum 125 characters")
+    String name;
+
+
+    @Column(length = 125)
+    @NotNull
+    @Size(max = 125, message = "description should be maximum 125 characters")
+    String description;
+
+
+    @Column(length = 125)
+    @NotNull
+    @Size(max = 125, message = "json should be maximum 125 characters")
+    String json;
+
+
+    @Column(length = 512)
+    @NotNull
+    @Size(max = 512, message = "template should be maximum 512 characters")
+    String template;
+
+
+    @Column(length = 125)
+    @NotNull
+    @Size(max = 125, message = "sqlStr should be maximum 125 characters")
+    String sqlStr;
+
+
+    @Column(length = 125)
+    @NotNull
+    @Size(max = 125, message = "sqlPart should be maximum 125 characters")
+    String sqlPart;
+
+
 }
