ed8104deac67427f59236c6951418d3bd658d9af
dlugi57 <dlugoszpeter@gmail.com>
2020-11-30 23:38:24 +0100

    CLEAN & configurration

---
+++ src/test/java/com/web/poseidon/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.web.poseidon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+
+    @Test
+    void main() {
+        Application.main(new String[] {});
+    }
+}
